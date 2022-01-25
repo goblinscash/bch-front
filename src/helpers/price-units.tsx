3fc94226c456d96a7a913839985dcf3c45bf55b5
trankuilized <57326781+trankuilized@users.noreply.github.com>
2022-01-25 10:37:18 -0500

    remove svg

--- src/helpers/price-units.tsx
+++ src/helpers/price-units.tsx
@@ -1,10 +1,10 @@
 import { SvgIcon } from "@material-ui/core";
-import { ReactComponent as MimImg } from "../assets/tokens/MIM.svg";
+//import { ReactComponent as FlexUsdImg } from "../assets/tokens/MIM.svg";
 import { IAllBondData } from "../hooks/bonds";
 import { fusd } from "../helpers/bond";
 
 export const priceUnits = (bond: IAllBondData) => {
-    if (bond.name === fusd.name) return <SvgIcon component={FusdIcon} viewBox="0 0 32 32" style={{ height: "15px", width: "15px" }} />;
+    //if (bond.name === flexusd.name) return <SvgIcon component={FlexUsdImg} viewBox="0 0 32 32" style={{ height: "15px", width: "15px" }} />;
 
     return "$";
 };
