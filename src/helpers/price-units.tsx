aae436930729bc2e255db642d0c75aa8ef2cfeec
trankuilized <57326781+trankuilized@users.noreply.github.com>
2022-01-31 15:53:50 -0500

    comment out fusd import

--- src/helpers/price-units.tsx
+++ src/helpers/price-units.tsx
@@ -1,7 +1,7 @@
 import { SvgIcon } from "@material-ui/core";
 //import { ReactComponent as FlexUsdImg } from "../assets/tokens/MIM.svg";
 import { IAllBondData } from "../hooks/bonds";
-import { fusd } from "../helpers/bond";
+//import { fusd } from "../helpers/bond";
 
 export const priceUnits = (bond: IAllBondData) => {
     //if (bond.name === flexusd.name) return <SvgIcon component={FlexUsdImg} viewBox="0 0 32 32" style={{ height: "15px", width: "15px" }} />;
