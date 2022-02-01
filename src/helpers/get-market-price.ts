6b2db17f593fb93b6d50e06b70f1a4f818d1b6e5
trankuilized <57326781+trankuilized@users.noreply.github.com>
2022-01-31 22:13:14 -0500

    marketprice

--- src/helpers/get-market-price.ts
+++ src/helpers/get-market-price.ts
@@ -7,6 +7,6 @@ export async function getMarketPrice(networkID: Networks, provider: ethers.Signe
     const fusdGobAddress = fusdGob.getAddressForReserve(networkID);
     const pairContract = new ethers.Contract(fusdGobAddress, LpReserveContract, provider);
     const reserves = await pairContract.getReserves();
-    const marketPrice = reserves[0] / reserves[1];
+    const marketPrice = reserves[1] / reserves[0];
     return marketPrice;
 }
